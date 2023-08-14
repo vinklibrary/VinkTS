6c1c455366bac59a8ea1eab3ddcd11e374f6bbdd
vinklibrary <shenxudong3@corp.netease.com>
2023-08-14 23:39:57 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+import os
+
+VERSION = '0.0.0'
+DESCRIPTION = 'Timeseries Package Awesome Tests'
+
+setup(
+    name = 'VinkTS',
+    version = VERSION,
+    author = 'sheldon shen',
+    author_email = 'shenxvdong1@gmail.com',
+    description = DESCRIPTION,
+
+)
